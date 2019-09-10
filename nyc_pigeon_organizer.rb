43adc65a6c16a683845a7f9f4da620c4fbf3e942
Adam La Rosa <adam.larosa@gmail.com>
2019-09-10 22:31:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.map { |key, value|
-    value.map { |attribute, names|
+    key.map { |attribute, names|
       names.map { |name|
         if !pigeon_list.has_key?(name)
           pigeon_list[name] = {}
