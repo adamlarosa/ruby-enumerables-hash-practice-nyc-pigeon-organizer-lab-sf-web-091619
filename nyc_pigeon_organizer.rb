8d65545998d06b56efd94f4655e9fcbe7f33dabd
Adam La Rosa <adam.larosa@gmail.com>
2019-09-10 23:06:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,7 +11,7 @@ def nyc_pigeon_organizer(data)
         if !pigeon_list.has_value?(key)
           pigeon_list[name][key] = []
         elsif
-          pigeon_list[name][key].push(attribute.to_s)
+          pigeon_list[name][key] = attribute.to_s
         end
         binding.pry
       }
