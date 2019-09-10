8e1c096ceb7cb93e09266b2ec7d33c2dcea99b29
Adam La Rosa <adam.larosa@gmail.com>
2019-09-10 21:39:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,11 +2,9 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   pigon_list = {}
-  data.map { |key, value, array|
+  data.map { |key, value|
     binding.pry
-    value.map { |key, value|
-     binding.pry
-    }
+    
   }
   pigon_list
 end
