50c7cc251bcfcda7c5a1fbd9580c1e8391066755
Adam La Rosa <adam.larosa@gmail.com>
2019-09-10 23:47:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,9 +7,10 @@ def nyc_pigeon_organizer(data)
       names.map { |name|
         if !pigeon_list.has_key?(name)
           pigeon_list[name] = {}
+          pigeon_list[name][key] = []
         end
         if !pigeon_list.has_value?(key)
-          pigeon_list[name][key] = attribute.to_s # THIS IS THE TROUBLE LINE
+          pigeon_list[name][key] << attribute.to_s # THIS IS THE TROUBLE LINE
         end
         binding.pry
       }
